d00f1a971f097d8598cd02aa1b0b841851bd6662
Andrew Rusu <andrewrusu87@hotmail.com>
2025-02-04 14:23:14 -0500

    Updated admin

--- challenge/challenge_api/admin.py
+++ challenge/challenge_api/admin.py
@@ -7,12 +7,19 @@ class ZillowListingAdmin(admin.ModelAdmin):
     list_display = ('id', 'zillow_id', 'address', 'bedrooms', 'bathrooms', 'price', 'property_size', 'area_unit')
     list_filter = ('home_type', 'area_unit', 'rentzestimate_last_updated')
     search_fields = ('id', 'zillow_id', 'street')
+    readonly_fields = ('id', 'created_at')
     date_hierarchy = 'created_at'
 
 
     def address(self, obj):
-        if obj is None:
-            return None
-
         return f'{obj.street}, {obj.city}, {obj.state} {obj.zipcode}'
     address.short_description = 'Address'
+
+
+    def get_readonly_fields(self, request, obj=None):
+        fields = super().get_readonly_fields(request, obj)
+
+        if obj:
+            return fields + ('zillow_id',)
+
+        return fields
